bdd0dbbc98515208e6ddfbad191ddeaa79d3ac40
Nicole Dwenger <nicole@famdw.dw>
2021-05-20 10:11:30 +0200

    venv add pydot

--- create_venv.sh
+++ create_venv.sh
@@ -6,6 +6,9 @@ python3 -m venv $VENVNAME
 source $VENVNAME/bin/activate
 pip install --upgrade pip
 
+pip install pydot
+pip install graphviz
+
 test -f requirements.txt && pip install -r requirements.txt
 
 deactivate
